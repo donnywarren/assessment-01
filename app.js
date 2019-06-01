7507ac62dc2cffe5beee43e2573bc86e7c7b4e1d
axylos <robertdraketalley@gmail.com>
2019-06-01 17:16:48 -0400

    adjust to arrow fns

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-function sum(a, b) {
+const sum = (a, b) => {
   // sum takes two arguments
   // and returns their sum
+
 }
 
-function genNums(n) {
+const genNums = (n) => {
   // genNums takes a single number as an argument
   // it should return an array of the numbers from 1..n
 
 }
 
-function greeter(name) {
+const greeter = (name) => {
   // greeter takes a string as an argument (a person's name)
   // it should return a string greeting the person
   // in the form of: Hello, <name>
 }
 
-function reverseString(str) {
+const reverseString = (str) => {
   // reverseString takes a string
   // and should return the reverse of the string
   // e.g., reverseString('cat') => 'tac'
 }
 
-function flipBool(arr) {
+const flipBool = (arr) => {
   // flipBool takes an array of Boolean values
   // it should call map on the array and flip
   // each Boolean value to its opposite,
